1b25862dffb043223c6fbee33ff8d551258212b2
Cavecake <simon.maxton@web.de>
2024-05-03 21:52:14 +0200

    Update sudoku.py
    Added Blocks to the valid movement check
    Uses specialised functions in valid move
    Adjusted number scale to 1 to 9
    New Sudoku

--- KillerSudoku/sudoku.py
+++ KillerSudoku/sudoku.py
@@ -12,14 +12,19 @@ class Sudoku():
         self.org_grid = grid
         self.__init_blocks()
     def valid_move(self, cell_x, cell_y, number):
-        if self.grid[cell_y][cell_x] != 0:
+        if self.org_grid[cell_y][cell_x] != 0:
             return False
         
-        for x in range(9):
-            if self.grid[cell_y][x] == number:
-                return False
-            if self.grid[x][cell_x] == number:
-                return False
+        block = self.get_block(cell_x,cell_y)
+        row = self.get_row(cell_y)
+        column = self.get_column(cell_x)
+        if number in column:
+            return False
+        if number in row:
+            return False
+        if number in block:
+            return False
+        
         return True
 
     def get_row(self, row_y):
@@ -43,38 +48,44 @@ class Sudoku():
             return True
         
         if self.org_grid[y][x] != 0:
-            x1 = (x +1) % 9
+            x1 = (x +1)
             y1 = y
             if x1 == 9:
                 x1 = 0
                 y1 = y + 1
-            return self.subSolver(x1,y1)
+            if self.subSolver(x1,y1):
+                return True
+            else:
+                self.grid[y][x] = 0
 
-        for i in range(9):
+        for i in range(1,10):
             if self.valid_move(x,y,i):
                 x1 = x +1
                 y1 = y
                 if x1 == 9:
                     x1 = 0
                     y1 = y + 1
+                self.grid[y][x] = i
                 if self.subSolver(x1,y1):
                     return True
-                
+                else:
+                    self.grid[y][x] = 0
+
         return False
 
     def dynamicSolver(self):
         self.subSolver(0,0)
         return self.grid
 sod = Sudoku([
-    [0,0,5,0,2,0,0,0,9],
-    [4,0,6,8,1,0,0,5,0],
-    [0,7,0,0,0,0,3,1,6],
-    [5,0,0,2,4,0,6,0,0],
-    [0,6,4,0,7,0,2,3,8],
-    [1,8,0,3,0,0,0,0,0],
-    [6,0,9,0,0,1,8,0,0],
-    [7,4,0,9,0,0,0,6,0],
-    [0,0,0,7,3,6,0,2,4]
+    [0,4,1,0,9,2,7,3,8],
+    [0,7,6,0,1,0,2,0,0],
+    [0,0,2,0,0,0,0,5,0],
+    [0,6,0,0,5,1,8,0,4],
+    [0,8,3,4,0,0,0,0,0],
+    [0,0,0,2,8,0,1,0,0],
+    [0,1,0,9,4,0,3,0,0],
+    [6,0,0,0,0,8,4,7,5],
+    [0,2,8,0,7,0,9,0,6]
 ])
 
 los = sod.dynamicSolver()
