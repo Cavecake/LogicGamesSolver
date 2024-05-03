67774ca994e4357e3fe2f9112ce4b44d54d45ea8
Cavecake <simon.maxton@web.de>
2024-05-03 23:10:46 +0200

    Update dynamicSolution.py
    Keyboard and mouse input

--- KillerSudoku/dynamicSolution.py
+++ KillerSudoku/dynamicSolution.py
@@ -23,6 +23,14 @@ class KillerSuoku(sudoku.Sudoku):
 
 class SudokuGUI():
     screen_res = (900,900)
+
+    SCREEN_COLOR = (0.5,0.5,0.5,1)
+    GRID_COLOR = (50,255,30)
+    SOLUTION_COLOR = (50,255,50,255)
+    NUMBER_COLOR = (0,0,0)
+    SELECTION_COLOR = (255,255,255)
+    SELECTION_OPACITY = 50
+
     def __init__(self) -> None:
         self.selectedCells = []
         self.grid = []
@@ -35,11 +43,20 @@ class SudokuGUI():
         self.solved = False
 
         self.gameWindow = pyglet.window.Window(width=900, height=900, resizable=True, vsync=True)
-        self.on_draw = self.gameWindow.event(self.on_draw)
         self.batch = pyglet.graphics.Batch()
-        pyglet.gl.glClearColor(0.5,0.5,0.5,1)
+        pyglet.gl.glClearColor(*self.SCREEN_COLOR)
+
+        self.multiselect = False
+
+        self.on_draw = self.gameWindow.event(self.on_draw)
+        self.on_mouse_press = self.gameWindow.event(self.on_mouse_press)
+        self.on_key_press = self.gameWindow.event(self.on_key_press)
+        self.on_key_release = self.gameWindow.event(self.on_key_release)
+        self.on_mouse_drag = self.gameWindow.event(self.on_mouse_drag)
+
     
     def drawGrid(self):
+        
         offset = 45
         lines = []
         WIDTH = 3
@@ -47,7 +64,7 @@ class SudokuGUI():
             w = WIDTH
             if x%3 == 0:
                 w += 5
-            line1 = pyglet.shapes.Line(offset+x*90, 45, offset+x*90, 855, w, color = (50, 225, 30), batch = self.batch)
+            line1 = pyglet.shapes.Line(offset+x*90, 45, offset+x*90, 855, w, color = self.GRID_COLOR, batch = self.batch)
             lines.append(line1)
             line1.opacity = 250
         
@@ -55,7 +72,7 @@ class SudokuGUI():
             w = WIDTH
             if y%3 == 0:
                 w += 5
-            line1 = pyglet.shapes.Line(45, offset+y*90, 855, offset+y*90, w, color = (50, 225, 30), batch = self.batch)
+            line1 = pyglet.shapes.Line(45, offset+y*90, 855, offset+y*90, w, color = self.GRID_COLOR, batch = self.batch)
             lines.append(line1)
             line1.opacity = 250
         
@@ -74,15 +91,27 @@ class SudokuGUI():
             for y in range(9):
                 number = self.grid[y][x]
                 if number != 0:
-                    self.drawLetter(x,y,labels,number,(0,0,0,1))
+                    self.drawLetter(x,y,labels,number,self.NUMBER_COLOR)
                     continue
                 if self.solved:
                     number = self.solved_grid[y][x]
-                    self.drawLetter(x,y,labels,number,(50,255,50,255))
+                    self.drawLetter(x,y,labels,number,self.SOLUTION_COLOR)
         return labels
+    
+    def drawSelection(self):
+        rects = []
+        for cell in self.selectedCells:
+            x, y = cell
+            rect = (46+x*90, 46+ y*90,
+                    88, 88)
+            rect = pyglet.shapes.Rectangle(rect[0], rect[1], rect[2], rect[3],self.SELECTION_COLOR, batch=self.batch)
+            rect.opacity = self.SELECTION_OPACITY
+            rects.append(rect)
+        return rects
     
     def drawElements(self):
         objects = []
+        objects.append(self.drawSelection())
         objects.append(self.drawGrid())
         objects.append(self.drawSudokuNumbers())
         return objects
@@ -97,14 +126,65 @@ class SudokuGUI():
         self.solved_grid = sud.dynamicSolver()
         self.solved = True
 
+    def calcCellCoord(self,x,y):
+        # Calc cell coord
+        x -= 45
+        y -= 45
+        x = x - x%90
+        y = y - y%90
+        x /= 90
+        y /= 90
+        if x > 8 or y > 8 or y < 0 or x < 0:
+            x, y = None, None
+        return x, y
+
+    def on_mouse_press(self,x, y, button, modifiers):
+        x, y = self.calcCellCoord(x,y)
+        if x == None:
+            return
+        if button == pyglet.window.mouse.LEFT:
+            if [int(x), int(y)] in self.selectedCells:
+                self.deselectCell(int(x),int(y))
+                return
+            if not self.multiselect:
+                self.deselectAll()
+            self.selectCell(int(x),int(y))
+        elif button == pyglet.window.mouse.RIGHT:
+            self.deselectCell(int(x),int(y))
+    
+    def on_key_press(self,symbol, modifiers):
+        if modifiers & pyglet.window.key.MOD_SHIFT:
+            self.setMutliselect(True)
+        number = pyglet.window.key.symbol_string(symbol)
+        number = number[-1]
+        if number in ["1","2","3","4","5","6","7","8","9"]:
+            self.setNumber(int(number))
+
+    def on_mouse_drag(self,x, y, dx, dy, buttons, modifiers):
+        x, y = self.calcCellCoord(x,y)
+        if x == None:
+            return
+        self.selectCell(int(x),int(y))
+
+    def on_key_release(self,symbol, modifiers):
+        key = pyglet.window.key.symbol_string(symbol)
+        if modifiers & pyglet.window.key.MOD_SHIFT or key[1:] == "SHIFT":
+            self.setMutliselect(False)
+
     def setNumber(self,number):
         for cell in self.selectedCells:
             x, y = cell
             self.grid[y][x] = number
 
+    def setMutliselect(self,state):
+        self.multiselect = state
+
     def selectCell(self,x,y):
         if [x,y] not in self.selectedCells:
             self.selectedCells.append([x,y])
+
+    def deselectAll(self):
+        self.selectedCells = []
 
     def deselectCell(self,x,y):
         if [x,y] in self.selectedCells:
